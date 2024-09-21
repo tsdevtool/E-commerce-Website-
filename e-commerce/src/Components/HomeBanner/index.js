fc266a6d61ce6624abc467577b5e2a3a3d2c2ba9
ThanhSieu2k3 <122494878+tsdevtool@users.noreply.github.com>
2024-09-21 21:23:07 +0700

    Done ex10

--- e-commerce/src/Components/HomeBanner/index.js
+++ e-commerce/src/Components/HomeBanner/index.js
@@ -1,44 +1,47 @@
-
 import React from "react";
 import Slider from "react-slick";
 
-const HomeBanner = ()=>{
-    var settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows:true,
-        autoplay:true
-      };
+const HomeBanner = () => {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+  };
 
-    return(
-        <div className="homeBannerSection">
-            <Slider {...settings}>
-                <div className="item">
-                    <img src="https://cmsimages.shoppersstop.com/main_strapi_web_fa797dee45/main_strapi_web_fa797dee45.png" 
-                    className="w-100"/>
-
-                </div>
-                <div className="item">
-                    <img src="https://cmsimages.shoppersstop.com/static_web_Forever_New_AND_and_more_b62fcda2ba/static_web_Forever_New_AND_and_more_b62fcda2ba.png" 
-                    className="w-100"/>
-
-                </div>
-                <div className="item">
-                    <img src="https://cmsimages.shoppersstop.com/static_web_Biba_Global_Desi_and_more_a0e91c950b/static_web_Biba_Global_Desi_and_more_a0e91c950b.png" 
-                    className="w-100"/>
-
-                </div>
-                <div className="item">
-                    <img src="https://cmsimages.shoppersstop.com/static_web_YSL_Burberry_and_more_a8595e6940/static_web_YSL_Burberry_and_more_a8595e6940.png" 
-                    className="w-100"/>
-                </div>
-            </Slider>
+  return (
+    <div className="homeBannerSection">
+      <Slider {...settings}>
+        <div className="item">
+          <img
+            src="https://cmsimages.shoppersstop.com/main_strapi_web_fa797dee45/main_strapi_web_fa797dee45.png"
+            className="w-100"
+          />
         </div>
-    )
-}
+        <div className="item">
+          <img
+            src="https://cmsimages.shoppersstop.com/static_web_Forever_New_AND_and_more_b62fcda2ba/static_web_Forever_New_AND_and_more_b62fcda2ba.png"
+            className="w-100"
+          />
+        </div>
+        <div className="item">
+          <img
+            src="https://cmsimages.shoppersstop.com/static_web_Biba_Global_Desi_and_more_a0e91c950b/static_web_Biba_Global_Desi_and_more_a0e91c950b.png"
+            className="w-100"
+          />
+        </div>
+        <div className="item">
+          <img
+            src="https://cmsimages.shoppersstop.com/static_web_YSL_Burberry_and_more_a8595e6940/static_web_YSL_Burberry_and_more_a8595e6940.png"
+            className="w-100"
+          />
+        </div>
+      </Slider>
+    </div>
+  );
+};
 
-export default HomeBanner
-
+export default HomeBanner;
--- e-commerce/src/Components/HomeCat/index.js
+++ e-commerce/src/Components/HomeCat/index.js
@@ -1,98 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 const HomeCat = () => {
+  const [itemBg, setItemBg] = useState([
+    "#fffceb",
+    "#ecffec",
+    "#feefea",
+    "#fff3eb",
+    "#fff3ff",
+    "#f2fce4",
+    "#feefea",
+    "#fffceb",
+    "#feefea",
+    "#ecffec",
+    "#feefea",
+    "#ffe3eb",
+    "#fff3ff",
+    "#f2fce4",
+    "#feefea",
+    "#fffceb",
+    "#feefea",
+    "#fff3eb",
+    "#fff3ff",
+    "#f2fce4",
+    "#feefea",
+    "#fffceb",
+    "#feefea",
+    "#ecffec",
+  ]);
+
   return (
     <section className="homeCat">
       <div className="container">
+        <h3 className="mb-3 hd">Featured Categories</h3>
         <Swiper
           slidesPerView={10}
           spaceBetween={8}
           navigation={true}
-          slidesPerGroup={1}
+          slidesPerGroup={3}
           modules={[Navigation]}
           className="mySwiper"
         >
-          <SwiperSlide>
-            <div className="item">
-              <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png" />
-              <h6>Red Apple</h6>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="item">
-              <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png" />
-              <h6>Red Apple</h6>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="item">
-              <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png" />
-              <h6>Red Apple</h6>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="item">
-              <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png" />
-              <h6>Red Apple</h6>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="item">
-              <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png" />
-              <h6>Red Apple</h6>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="item">
-              <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png" />
-              <h6>Red Apple</h6>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="item">
-              <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png" />
-              <h6>Red Apple</h6>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="item">
-              <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png" />
-              <h6>Red Apple</h6>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="item">
-              <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png" />
-              <h6>Red Apple</h6>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="item">
-              <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png" />
-              <h6>Red Apple</h6>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="item">
-              <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png" />
-              <h6>Red Apple</h6>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="item">
-              <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png" />
-              <h6>Red Apple</h6>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="item">
-              <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png" />
-              <h6>Red Apple</h6>
-            </div>
-          </SwiperSlide>
+          {itemBg?.map((item, index) => {
+            return (
+              <SwiperSlide>
+                <div className="item cursor" style={{ background: item }}>
+                  <img src="https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/cat-9.png" />
+                  <h6>Red Apple</h6>
+                </div>
+              </SwiperSlide>
+            );
+          })}
         </Swiper>
       </div>
     </section>
